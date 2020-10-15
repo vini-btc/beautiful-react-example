ed76065df33b80eb1afe7552ec6f876cd63d9019
Vini Andrade <vini.andrade.dev@gmail.com>
2020-10-15 07:59:14 +0200

    Introduce hyperscript
    Signed-off-by: Vini Andrade <vini.andrade.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
 import React from 'react';
+import h from 'react-hyperscript';
+import hh from 'hyperscript-helpers';
 import './App.css';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
+const { button, div, ol, li } = hh(h);
+
+function Square({ onClick, value }) {
+  return button('.square', { onClick }, value);
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
+    return h(Square, { value: this.props.squares[i], onClick: () => this.props.onClick(i) });
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return div([
+      div('.board-row', [this.renderSquare(0), this.renderSquare(1), this.renderSquare(2)]),
+      div('.board-row', [this.renderSquare(3), this.renderSquare(4), this.renderSquare(5)]),
+      div('.board-row', [this.renderSquare(6), this.renderSquare(7), this.renderSquare(8)]),
+    ]);
   }
 }
 
@@ -91,11 +72,7 @@ class Game extends React.Component {
       const desc = move ?
         'Go to move #' + move :
         'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
+      return li({ key: move }, button({onClick: () => this.jumpTo(move)}, desc));
     });
 
     let status;
@@ -105,20 +82,19 @@ class Game extends React.Component {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={i => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
+    return div('.game', [
+      div('.game-board', [
+        h(Board,
+          { squares: current.squares,
+            onClick: i => this.handleClick(i)
+          }
+        )
+      ]),
+      div('.game-info', [
+        div(status),
+        ol(moves)
+      ])
+    ]);
   }
 }
 
